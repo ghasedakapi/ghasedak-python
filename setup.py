0bd659e2164ee822dc1d86f997c69bfadb88f7ff
Dariush Abbasi <dariushem@yahoo.com>
2018-07-21 13:40:37 +0430

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ghasedak',
+      version='0.0.1',
+      description='Ghasedak sms webservice api wrapper for python.',
+      url='http://github.com/ghasedakapi/ghasedak-python',
+      author='Dariush Abbasi',
+      author_email='poshtehani@gmail.com',
+      license='BSD-3-Clause-Clear',
+      packages=['ghasedak'],
+      zip_safe=False)
