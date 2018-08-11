8b98db0201ce79e9ca73c4f602370aadefe126cd
Dariush Abbasi <dariushem@yahoo.com>
2018-08-11 09:39:36 +0430

    basic structure

---
+++ ghasedak/ghasedak.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+	ghasedak.py
+	~~~~~~~~~
+	:copyright: (c) 2018 by Ghasedak ICT
+	:license: BSD-3-Clause-Clear, see LICENSE for more details.
+"""
+
+class Ghasedak:
+    """docstring for Ghasedak."""
+    def __init__(self, apikey):
+        self.apikey = apikey
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Ghasedak-python
+
+Ghasedak sms webservice Python package.
+
+## installation
+
+```shell
+pip install ghasedak
+```
+
+## usage
+
+Import `ghasedak` package.
+
+```python
+import ghasedak
+```
+
+## license
+
+Released under the BSD-3-Clause-Clear License.
